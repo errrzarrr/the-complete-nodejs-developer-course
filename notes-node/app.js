cad0411ac1c7d63a69b5ae42adae01af92f5349e
Roberto <errrzarrr@Gmail.com>
2018-07-05 16:55:28 -0400

    Package.json created, added notes.js

--- notes-node/app.js
+++ notes-node/app.js
@@ -1,13 +1,17 @@
 const fs = require('fs');
 const os = require('os');
+const notes = require('./notes.js');
 const FILENAME = 'greetings.txt';
 
 var userInfo = os.userInfo();
-var msg = `Hello ${userInfo.username}!`;
+var msg = `Hello ${userInfo.username}! Current year is ${notes.year}`;
 
+console.log('starting app.js');
 fs.appendFile(FILENAME, msg,(err)=>{
     if(err)
         console.log('an error has occurred:', err);
     else
         console.log('File successfully saved');
 });
+
+console.log( 'result: '+notes.add(1,7) );
---
+++ notes-node/notes.js
@@ -0,0 +1,10 @@
+console.log('starting notes.js');
+module.exports.year = 2018;
+
+module.exports.addNote = () => {
+    console.log('addNote');
+    return 'new note';
+}
+
+module.exports.add = (a, b) => a+b
+console.log(module);
