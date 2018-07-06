73d34015474e268e210dcbf109c2331cc6a3c736
Roberto <errrzarrr@Gmail.com>
2018-07-06 17:17:08 -0400

    package.json & app.js
    added yargs to package.json & arg proccessing on app.js

--- notes-node/app.js
+++ notes-node/app.js
@@ -8,10 +8,24 @@ var msg = `Hello ${userInfo.username}! Current year is ${notes.year}`;
 
 console.log('starting app.js');
 fs.appendFile(FILENAME, msg,(err)=>{
-    if(err)
-        console.log('an error has occurred:', err);
-    else
-        console.log('File successfully saved');
+	if(err)
+		console.log('an error has occurred:', err);
+	else
+		console.log('File successfully saved');
 });
-
 console.log( 'result: '+notes.add(1,7) );
+
+var command = process.argv[2];
+
+
+if(command === 'add')
+	console.log('adding a new note');
+else if(command === 'list')
+	console.log('listing all notes');
+else if(command === 'read')
+	console.log('reading note');	
+else if(command === 'remove')
+	console.log('removing note');	
+else 
+	console.log(`command '${command}' not recognized`);
+
