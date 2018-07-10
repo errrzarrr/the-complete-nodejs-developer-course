9756c5b234503ccd7760b5726ce9422c11b8adbd
Roberto M @ASUS Laptop <errrzarrr@gmail.com>
2018-07-09 22:27:24 -0400

    saving playground/json.js

---
+++ notes-node/playground/json.js
@@ -0,0 +1,17 @@
+const fs = require('fs');
+const FILENAME = 'notes.json';
+
+var noteObj = {
+	title: 'the title'
+	,body: 'here goes the note body'
+};
+var noteAsString 
+	= JSON.stringify( noteObj );
+
+fs.writeFileSync(FILENAME, noteAsString);
+
+var noteFromFile 
+	= fs.readFileSync( FILENAME );
+var note = JSON.parse(noteFromFile);
+console.log(typeof note);
+console.log(note.title);
