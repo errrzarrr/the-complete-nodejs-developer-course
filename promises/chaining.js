adc1ae32e13d9322af1ee20aeab8f564497f8dde
Roberto M @ASUS Laptop <errrzarrr@gmail.com>
2018-07-22 18:30:58 -0400

    promises chaining fixed
    FIXed getRandomInt function: Not excluding upper bound anymore.
    Now code does chaining properly.

--- promises/chaining.js
+++ promises/chaining.js
@@ -1,30 +1,36 @@
 // chaining those promises.
 
 const getRandomInt 
-	= (min, max) =>  Math.floor(Math.random() * (max - min)) + min;
+	= (min, max) =>  Math.floor(Math.random() * (++max - min)) + min;
 
 const placeBet = (amount) => {
 	var boolRand 
-		= getRandomInt(0,2)  === 1 ? true : false;
+		= getRandomInt(0,1)  === 1 ? true : false;
 	return new Promise( (resolve, reject) => {
 		if(boolRand)
-			resolve(amount);
+			resolve({amount, msg: 'Joined the bet'});
 		else
 			reject( 'Bets not open now' );
 	});
 };
 
-const  throwDice = (guess) => {
+const  throwDice = (guess, bet) => {
 	var dice = getRandomInt(1,6);
+	console.log(`${bet.msg}. Threw dice attemping a [${guess}] and got [${dice}]`);
 	return new Promise( (resolve, reject) => {
 		if( guess === dice)
-			resolve('succeed');
+			resolve({ amount: bet.amount, msg:'Succeed'});
 		else
-			reject('try again');
+			reject('Dice missed. Try again');
 	});
 };
 
+const betWon 
+	= (res) => console.log(`${res.msg}! Won a bet of ${res.amount}$`)
+
+const handleReject = (rej) => console.log(rej);
+
 placeBet(5000)
-	.then( () => {return throwDice(1)} )
-	.then( (res) => console.log(res) )
-	.catch( (res) => console.log(res) );
+	.then( (res) => { return throwDice(1, res) })
+	.then( (res) => betWon(res) )
+	.catch( (rej) => handleReject(rej) );
