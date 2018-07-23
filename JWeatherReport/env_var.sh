be53b3d3a7d777f43cf0795907023c25698ddb35
Roberto <errrzarrr@Gmail.com>
2018-07-23 09:08:09 -0400

    bash script for keys improved

--- JWeatherReport/env_var.sh
+++ JWeatherReport/env_var.sh
@@ -1,8 +1,8 @@
-# export GEOKEY='...';
-# echo $GEOKEY;
+FILENAME='key.txt';
+export GEOKEY=$( sed -n '1p' $FILENAME ); 
+echo 'GEOKEY set';
 # To retrieve it from node: process.env.GEOKEY;
-# export FORECASTKEY='';
-# echo $FORECASTKEY;
-# To retrieve it from node: process.env.FORECASTKEY;
 
- 
+export FORECASTKEY=$( sed -n '2p' $FILENAME ); 
+echo 'FORECASTKEY set';
+# To retrieve it from node: process.env.FORECASTKEY;
