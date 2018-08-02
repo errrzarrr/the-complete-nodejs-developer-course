8a5f15991a15ff214ce7fb950b9224185aad99da
Roberto M @ASUS Laptop <errrzarrr@gmail.com>
2018-08-02 18:22:48 -0400

    todo app : db update functionalities

---
+++ todo-app/db/update.js
@@ -0,0 +1,32 @@
+const {MongoClient, ObjectId} = require('mongodb');
+
+let objId =  new ObjectId();
+const URL = `mongodb://localhost:27017`;
+let dbName = 'todoApp';
+
+MongoClient.connect(URL, (err, client) => {
+	if(err)
+		return console.log('Unable to connect to MongoDB server');
+		
+	console.log('Connected to MongoDB server');
+	var dbo = client.db(dbName);
+	
+	// findOneAndUpdate(filter, update)
+	dbo.collection('users').findOneAndUpdate( {name: "Andy"}, { $set:{name:"Andy V"}, $inc:{age:1} })
+		.then((result) => {
+			console.log(result);
+		})
+		.catch((err) =>
+			console.log('unable to update users', err)
+		);
+	// findOneAndUpdate(filter, update, options)
+	// returnOriginal:false returns the updated doc instead of the older one
+	dbo.collection('users').findOneAndUpdate( {location: /Liberty Street/i}, {$set:{name:"Voluntary Street"}}, {returnOriginal:false})
+		.then((result) => {
+			console.log(result);
+		})
+		.catch((err) =>
+			console.log('unable to update users', err)
+		);	
+	client.close();	
+});
