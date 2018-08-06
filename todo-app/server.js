ded7c119855b0862364fb62dda614d2f15d1fefb
Roberto M @ASUS Laptop <errrzarrr@gmail.com>
2018-08-06 18:09:04 -0400

    todo-app: POSTs methods working
    POSTs methods working for /todo & /user
    NPM: added body-parser
    also: mongoose/server.js renamed as mongoose/example_server.js

---
+++ todo-app/server.js
@@ -0,0 +1,38 @@
+let express = require('express');
+let app = express();
+const PORT = process.env.WEBSERVER_PORT || 3000;
+
+var bodyParser = require('body-parser');
+let mongoose = require('mongoose');
+let config = require('./db/dbconfig');
+var {Todo} = require('./db/models/Todo');
+var {User} = require('./db/models/User');
+
+let todo;
+let user;
+// app.use( bodyParser.json({type:'*/*'}) ); 
+app.use( bodyParser.json({type:'application/json'}) );
+
+app.post('/todo', (req, res) => {
+    console.log('POST /todo recieved body: ', req.body);
+    todo =
+        new Todo({text:req.body.text, completed:req.body.completed, completedAt:req.body.completedAt});
+    todo.save()
+        .then( (doc) => res.status(201).send(doc) )
+        .catch( (error) => res.status(400).send(`${error.name}: ${error.message}`) ); 
+});
+
+app.post('/user', (req, res) => {
+    console.log('POST /user received body: ', req.body);
+    user = 
+        new User({name:req.body.name, email:req.body.email, location:req.body.location, age:req.body.age});
+    user.save()
+        .then( (doc) => res.status(201).send(doc) )
+        .catch( (error) => res.send(`${error.name}: ${error.message}`) ); 
+});
+
+app.listen(PORT, () =>
+    console.log(`todo-app server up & running on port ${PORT}`)
+);
+
+module.exports = {app};
