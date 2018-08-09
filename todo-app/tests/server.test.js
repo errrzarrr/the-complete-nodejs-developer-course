d1235b4e5dd0d8e1d695e6d2af0433e2fe3ec863
Roberto M @ASUS Laptop <errrzarrr@gmail.com>
2018-08-08 22:19:32 -0400

    todo-app: tests
    it was hard, uf! but at last made tests work

--- todo-app/tests/server.test.js
+++ todo-app/tests/server.test.js
@@ -4,9 +4,18 @@ const supertest = require('supertest');
 const {app} = require('../server');
 const {Todo} = require('../db/models/Todo');
 
+const testTodos = [
+	{text: 'test todo #1'}
+	,{text: 'test todo #1'}
+];
+
 // WARNING: This wipes out named collection. A testing DB is needed for this.
 // since our DB already has 'todo' docs in it, this method --for test purposes-- wipes them out, so we can 'expect(todos.length).toBe(1)'
-beforeEach( (done) => Todo.remove({}).then(() => done()) );
+beforeEach( (done) => {
+	Todo.remove({})
+		.then( () => {return Todo.insertMany(testTodos) })
+		.then(() => done());
+});
 
 describe('POST /todo', () => {
 	it('should create a new todo', (done) => {
@@ -22,7 +31,7 @@ describe('POST /todo', () => {
 				if(err)
 					done(err);
 				else {
-					Todo.find()
+					Todo.find({text})
 						.then((todos) => {
 							expect(todos.length).toBe(1);
 							expect(todos[0].text).toBe(text);
@@ -43,12 +52,31 @@ describe('POST /todo', () => {
 				else {
 					Todo.find()
 						.then((todos) => {
-							expect(todos.length).toBe(0);
+							expect(todos.length).toBe(testTodos.length);
 							done();
 						})
 						.catch((e) => done(e));
 				}
 			});
 	});
+});
 
+describe('GET /todo', () => {
+	it('should retrieve all todos', (done) => {
+		supertest(app)
+			.get('/todo') 
+			.expect(200)
+			.end((err, res) => {
+				if(err)
+					done(err);
+				else {
+					Todo.find({})
+						.then((todos) => {
+							expect(todos.length).toBe(testTodos.length);
+							done();
+						})
+						.catch((e) => done(e));
+				}
+			});
+	});
 });
