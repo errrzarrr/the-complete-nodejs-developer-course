578ee70d5cbf8a4ef2987ae318cfcbab359a35fb
Roberto M @ASUS Laptop <errrzarrr@gmail.com>
2018-08-23 14:11:33 -0400

    an improved/cleaner way of configuring env vars

--- todo-app/config/env_var.js
+++ todo-app/config/env_var.js
@@ -1,16 +1,16 @@
 /*
-  this configs and switches development and testing enviroments
-  so development or production DBs aren't affected by tests.
-  In heroku or any other production server this is set to 'production'
-  and won't be executed.
+	this configs and switches development and testing enviroments
+	so development or production DBs aren't affected by tests.
+	In heroku or any other production server this is set to 'production'
+	and won't be executed.
 */
+
 const ENV = process.env.NODE_ENV || 'development';
-switch(ENV) {
-	case 'development':
-		process.env.ENV_TABLE = 'todoApp';
-		break;
-	case 'test':
-		process.env.ENV_TABLE = 'todoApp_test';
-		break;
-	default: break;
+
+
+if(ENV === 'development' || ENV === 'test') {
+	const config = require('./config.json');
+	var envConfig = config[ENV];
+	Object.keys(envConfig)
+		.forEach( key => process.env[key] = envConfig[key] );
 }
