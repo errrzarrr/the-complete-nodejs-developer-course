c059d654d92d680bd3e4afbc00b4e85bf4e65519
Roberto M @ASUS Laptop <errrzarrr@gmail.com>
2018-09-11 14:29:53 -0400

    adding form to front end

--- chatatcloud/public/js/index.js
+++ chatatcloud/public/js/index.js
@@ -17,7 +17,18 @@ socket.on('connect', function()  {
 
 socket.on('newMessage', (message) => {
 	console.log('newMessage', message);
+	var li = $('<li></li>');
+	li.text(`${message.from}: ${message.text}`);
+	$('#messages').append(li);
+});
+$('#message-form').on('submit', function(e){
+	e.preventDefault();
+	socket.emit('createMessage' 
+		,{ from: 'User', text: $('[name=message]').val() }
+		,() => {}
+	);
 });
 socket.on('disconnect', () => {
 	console.log('Disconnected from server');
 });
+
