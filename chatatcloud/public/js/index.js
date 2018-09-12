f2a87e4440258bbf906db8815e46c303c13c7342
Roberto M @ASUS Laptop <errrzarrr@gmail.com>
2018-09-12 13:41:11 -0400

    Chat UI now has CSS

--- chatatcloud/public/js/index.js
+++ chatatcloud/public/js/index.js
@@ -46,13 +46,17 @@ var locationButton = $('#send-location');
 locationButton.on('click',function() {
 	if(!navigator.geolocation)
 		return alert('Geolocation not supported by your browser');
+
+	locationButton.attr('disabled', 'disabled').text('Sending...');
 	navigator.geolocation.getCurrentPosition(
 		function(position) {
+			locationButton.removeAttr('disabled').text('Send location');
 			socket.emit('createLocationMessage'
-				,{latitude: position.coords.latitude	,longitude: position.coords.longitude}
+				,{latitude: position.coords.latitude ,longitude: position.coords.longitude}
 			);
 		}
 		,function() {
+			locationButton.removeAttr('disabled').text('Send location');
 			alert('Unable to fetch location'); 
 	});
 });
--- chatatcloud/server/utils/message.test.js
+++ chatatcloud/server/utils/message.test.js
@@ -2,24 +2,24 @@ var expect = require('expect');
 var {generateMessage, generateLocationMessage} = require('./message');
 
 describe('generateMessage', () => {
-    it('should generate the correct message object', () => {
-        var from =  'tester';
-        var text = 'hello'; 
-        let message = generateMessage(from, text);
-        
-        expect(message).toInclude({ from, text });
-        expect(message.createdAt).toBeA('number');
-    });    
+	it('should generate the correct message object', () => {
+		var from =  'tester';
+		var text = 'hello'; 
+		let message = generateMessage(from, text);
+		
+		expect(message).toInclude({ from, text });
+		expect(message.createdAt).toBeA('number');
+	});    
 });
 describe('generateLocationMessage', () => {
-    it('should generate correct location object', () => {
-        var from =  'tester';
-        var coord = {latitude: 1.1, longitude:1.2};
-        var url = `https://www.google.com/maps?q=${coord.latitude},${coord.longitude}`;
+	it('should generate correct location object', () => {
+		var from =  'tester';
+		var coord = {latitude: 1.1, longitude:1.2};
+		var url = `https://www.google.com/maps?q=${coord.latitude},${coord.longitude}`;
 
-        let message = generateLocationMessage(from, coord.latitude, coord.longitude);
-        
-        expect(message).toInclude({ from, url });
-        expect(message.createdAt).toBeA('number');
-    });    
+		let message = generateLocationMessage(from, coord.latitude, coord.longitude);
+		
+		expect(message).toInclude({ from, url });
+		expect(message.createdAt).toBeA('number');
+	});    
 });
